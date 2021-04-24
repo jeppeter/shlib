8e41d142da87fb34080164384370a3ba26ae42ad
jeppeter <jeppeter@gmail.com>
2021-04-24 15:17:00 +0800

    to add cloneall

---
+++ gitclone/cloneall.sh
@@ -0,0 +1,41 @@
+function clone_private_repo()
+{
+	local _repo=$1;
+	local _br=$2;
+	local _dir=$3;
+	local _ro2=`echo "$_repo" | sed 's/gitee.com/bitbucket.org/'`;
+	local _ro3=`echo "$_repo" | sed 's/gitee.com/gitlab.com/'`;
+
+	if [ ! -d "$_dir" ]
+	then
+		gitclonesucc "$_repo";
+		pushd $PWD && cd "$_dir" && git co "$_br" && git remote add origin2 "$_ro2" && \
+		git remote add origin3 "$_ro3" && popd;
+	fi
+}
+
+function clone_public_repo()
+{
+	local _repo=$1;
+	local _br=$2;
+	local _dir=$3;
+	local _ro2=`echo "$_repo" | sed 's/github.com/bitbucket.org/'`;
+
+	if [ ! -d "$_dir" ]
+	then
+		gitclonesucc "$_repo";
+		pushd $PWD && cd "$_dir" && git co "$_br" && git remote add origin2 "$_ro2" && \
+		popd;
+	fi
+}
+
+function clone_repo()
+{
+	local _repo=$1;
+	local _dir=$2;
+
+	if [ ! -d "$_dir" ]
+	then
+		gitclonesucc "$_repo";
+	fi
+}
